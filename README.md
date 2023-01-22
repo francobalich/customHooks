e47d36b4a3007e38e46ba4276779872affbc7d01
Franco Balich <franco.balich@gmail.com>
2023-01-22 18:14:14 -0300

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom Hooks
+
+En este repositorio podran ver algunos custom hooks que fue creando con el tiempo.
---
+++ useTodos.js
@@ -0,0 +1,46 @@
+import { useReducer, useEffect } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const initialState = []
+const init = ()=>{
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodos = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(()=>{
+        localStorage.setItem('todos', JSON.stringify(todos))
+    },[todos])
+
+    const handleNewTodo=(todo)=>{
+        const action = {
+            type:"[TODO] Add Todo",
+            payload:todo
+        }
+        dispatch(action)
+    }
+    const handleRemoveTodo=(todo)=>{
+        const action = {
+            type:"[TODO] Remove Todo",
+            payload:todo
+        }
+        dispatch(action)
+    }
+    const handleTonggleTodo = (todo)=>{
+        const action = {
+            type:"[TODO] Tonggle Todo",
+            payload:todo
+        }
+        dispatch(action)
+    }
+
+    return {
+        todos,
+        todosCount:todos.length,
+        pendingTodosCount:todos.filter(todo=> !todo.done).length,
+        handleNewTodo,
+        handleTonggleTodo,
+        handleRemoveTodo
+    }
+}
